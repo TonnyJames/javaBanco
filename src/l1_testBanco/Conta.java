b9e9cfedf42d03bd19c597945eafe6137f0ed50b
Tonny James P. dos Reis <tonny.jreis@outlook.com>
2021-11-08 13:31:32 -0300

    alter 2

--- src/l1_testBanco/Conta.java
+++ src/l1_testBanco/Conta.java
@@ -1,6 +1,5 @@
 package l1_testBanco;
 
-import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 
@@ -8,18 +7,15 @@ public class Conta {
 
 	private int numConta = 20215511;
 	private int numBanco, numAgencia;
-	private String nomeAgencia, nomeBanco, tipoConta;	
+	private String nomeAgencia, nomeBanco, tipoConta;
 	private int chooseBank;
 	private int chooseType;
 	private double saldo, valSaq, valDep;
-	
-	Scanner scan = new Scanner(System.in);
-	
-	// Getters and Setters
-	
 
-	
-	
+	Scanner scan = new Scanner(System.in);
+
+	// Getters and Setters
+
 	public int getNumConta() {
 		return numConta;
 	}
@@ -91,7 +87,7 @@ public class Conta {
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-	
+
 	public double getValSaq() {
 		return valSaq;
 	}
@@ -108,122 +104,89 @@ public class Conta {
 		this.valDep = valDep;
 	}
 
-	public void abrir () {
-		
+	public void abrir() {
+
 		System.out.println("Bem-vindo ao Tonnybank 24hs");
 		System.out.println("Digite 1 para Banco 1 \nDigite 2 para banco 2 \nDigite 3 para banco 3");
 		this.setChooseBank(scan.nextInt());
 		if (chooseBank == 1) {
 			this.setNomeBanco("banco 1");
-			this.setNumBanco(101);	
-		}else if (chooseBank == 2) {
+			this.setNumBanco(101);
+		} else if (chooseBank == 2) {
 			this.setNomeBanco("banco 2");
-			this.setNumBanco(102);	
-		}else if (chooseBank == 3) {
+			this.setNumBanco(102);
+		} else if (chooseBank == 3) {
 			this.setNomeBanco("banco 3");
-			this.setNumBanco(103);	
-		}else {
+			this.setNumBanco(103);
+		} else {
 			System.out.println("Banco inexistente");
 		}
-		
-		System.out.println("Digite 1 para conta-corrente \nDigite 2 para conta-poupança \nDigite 3 para conta conjunta");
+
+		System.out
+				.println("Digite 1 para conta-corrente \nDigite 2 para conta-poupança \nDigite 3 para conta conjunta");
 		this.setChooseType(scan.nextInt());
 		if (chooseType == 1) {
 			this.setTipoConta("Corrente");
-		}else if (chooseType == 2) {
+		} else if (chooseType == 2) {
 			this.setTipoConta("Poupança");
-		}else if (chooseType == 3) {
+		} else if (chooseType == 3) {
 			this.setTipoConta("Conjunta");
-		}else {
+		} else {
 			this.setTipoConta("Encerrada");
 		}
-		
+
 		System.out.println("Digite 1 para Agencia 111  \nDigite 2 para Agencia 222 \nDigite 3 para Agencia 333");
 		int chooseAgencia = scan.nextInt();
 		if (chooseAgencia == 1) {
 			this.setNumAgencia(111);
-		}else if (chooseType == 2) {
+		} else if (chooseType == 2) {
 			this.setNumAgencia(222);
-		}else if (chooseType == 3) {
+		} else if (chooseType == 3) {
 			this.setNumAgencia(333);
-		}else {
+		} else {
 			this.setTipoConta("Agencia invalida!!!");
 		}
-		
+
 		numConta += 1;
 		this.setSaldo(0);
-		
+
 		System.out.println("Sua conta foi criada com sucesso");
-		
+
 		check();
-		
-		
+
 	}
-	
+
 	public void check() {
-		
-		if(this.saldo < 0) {
+
+		if (this.saldo < 0) {
 			this.setTipoConta("Encerrada");
-		System.out.println("Esta conta foi e ENCERRADA por saldo negativo");
-	}else {
-		System.out.println("Conta: "+ this.getNumConta() + "\nAgência:"+ this.getNumAgencia()+ "\nSaldo atual:"+ this.getSaldo());
-		System.out.println("Horario da verificação"+ LocalDateTime.now());
+			System.out.println("Esta conta foi e ENCERRADA por saldo negativo");
+		} else {
+			System.out.println("Conta: " + this.getNumConta() + "\nAgência:" + this.getNumAgencia() + "\nSaldo atual:"
+					+ this.getSaldo());
+			System.out.println("Horario da verificação" + LocalDateTime.now());
+		}
 	}
-	}
-	
+
 	public void creditar() {
 		if (tipoConta != "Encerrada") {
 			System.out.println("Digite o valor que deseja DEPOSITAR:");
 			this.setSaldo(this.saldo += scan.nextDouble());
 			System.out.println("Deposito efetualdo com sucesso!!! \nVerificando saldo...");
 			check();
-		}else {
+		} else {
 			System.out.println("Esta conta foi encerrada por Saldo negativo");
-		}		
+		}
 	}
-	
+
 	public void debitar() {
 		if (tipoConta != "Encerrada") {
 			System.out.println("Digite o valor que deseja RETIRAR:");
 			this.setSaldo(this.saldo -= scan.nextDouble());
 			System.out.println("Saque efetualdo com sucesso!!! \nVerificando saldo...");
 			check();
-		}else {
+		} else {
 			System.out.println("Esta conta foi encerrada por Saldo negativo");
-		}		
+		}
 	}
-	
-	
-//	public void menu() {
-//		int opt = -1;
-//		
-//		do {
-//		System.out.println("\nSelecione uma ação \n**********************" );
-//		
-//		System.out.println("Abrir uma conta digite 1 \nConsultar Saldo digite 2 \nDepositar digite 3 \nSaque digite 4 \nPARA SAIR 0");
-//		opt = scan.nextInt();
-//		
-//		
-//		switch(opt) {
-//		case 1: abrir();
-//		break;
-//		
-//		case 2: check();
-//		break;
-//		
-//		case 3: creditar();
-//		break;
-//		
-//		case 4: debitar();
-//		break;
-//		
-//		default: break;			
-//		}
-//		
-//		}while(opt != 0);
-//	}
-	
-	}
-
-
-
+}
--- src/l1_testBanco/ContaTester.java
+++ src/l1_testBanco/ContaTester.java
@@ -5,36 +5,36 @@ import java.util.Scanner;
 public class ContaTester {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 		int opt = -1;
-		
+
 		Conta cliente = new Conta();
-		
-		
+
 		System.out.println("***Bem-vindo ao Tonnybank 48hs***");
-		
+
 		do {
-		System.out.println("\nSelecione uma ação \n**********************" );
-		
-		System.out.println("Abrir uma conta digite 1 \nConsultar Saldo digite 2 \nDepositar digite 3 \nSaque digite 4 \nPARA SAIR 0");
-		opt = scan.nextInt();
-		
-		if(opt == 1){
-			cliente.abrir();
-		}else if(opt == 2){
-			cliente.check();
-		}else if(opt == 3) {
-			cliente.creditar();
-		}else if(opt == 4) {
-			cliente.debitar();
-		}else if(opt == 0) {
-			break;
-		}
-		}while(opt != 0);
-		
+			System.out.println("\nSelecione uma ação \n**********************");
+
+			System.out.println(
+					"Abrir uma conta digite 1 \nConsultar Saldo digite 2 \nDepositar digite 3 \nSaque digite 4 \nPARA SAIR 0");
+			opt = scan.nextInt();
+
+			if (opt == 1) {
+				cliente.abrir();
+			} else if (opt == 2) {
+				cliente.check();
+			} else if (opt == 3) {
+				cliente.creditar();
+			} else if (opt == 4) {
+				cliente.debitar();
+			} else if (opt == 0) {
+				break;
+			}
+		} while (opt != 0);
+
 		System.out.println("Obrigado!!! volte sempre ou não, vc que sabe");
-		
+
 		scan.close();
 	}
 
